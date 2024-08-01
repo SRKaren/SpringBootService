80241f93aebc8316136efb10e502a427ac6bfbf5
Karen Silva Ramirez <extksilva@transbank.cl>
2024-08-01 16:17:40 -0600

    Adding tests

--- src/main/java/com/darckcode/spring/app/DemoApplication.java
+++ src/main/java/com/darckcode/spring/app/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.darckcode.spring.app.Controllers;
+package com.darckcode.spring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
